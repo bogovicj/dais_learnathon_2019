15a7bbe08d32cdcc39c8b62b046937693072dbd4
bogovicj <bogovicj@janelia.hhmi.org>
2019-06-23 07:25:00 -0400

    example 1 edits
    * add exercise
    * clean up
    * only read boats image from the web once

--- src/main/java/net/imglib2/examples/Ex01_TranformPointsAndImages.java
+++ src/main/java/net/imglib2/examples/Ex01_TranformPointsAndImages.java
@@ -44,15 +44,28 @@ public class Ex01_TranformPointsAndImages
 		RandomAccessibleInterval< UnsignedByteType > boatsImage = example.helper.openBoats();
 		example.helper.displayImageAndPoint( boatsImage, example.p, "Boats" );
 
-		// translate the image by a fixed number of pixels
-		example.simpleImageTransform( boatsImage );
+		/* 
+		 * translate the image by a fixed number of pixels
+		 */
+		//example.simpleImageTransform( boatsImage );
 
-		// do it again, but now learn to apply the transform to points	
+		/* 
+		 * do it again, but now learn to apply the transform to points
+		 */
 		example.discreteTransformImageAndPoint( boatsImage );
 
-		// build a more complex, continuous transformation
-		// and apply it to the image, and to a point
-		example.realTransform( boatsImage );
+		/*
+		 * build a more complex, continuous transformation
+		 * and apply it to the image, and to a point
+		 */
+		//example.realTransform( boatsImage );
+
+		/*
+		 * If you'd like to test yourself, fill in the 
+		 * method "resample2d" below, then uncomment the line 
+		 * below, and run
+		 */
+		//example.testResample2d();
 	}
 
 	/**
@@ -60,7 +73,12 @@ public class Ex01_TranformPointsAndImages
 	 */
 	public < T extends RealType< T > > void simpleImageTransform( RandomAccessibleInterval< T > img )
 	{
-		ij.ui().show( "Translated boats", Views.interval( Views.translate( Views.extendZero( img ), translation ), img ) );
+		ij.ui().show( "Translated boats", 
+				Views.interval( 
+						Views.translate( 
+								Views.extendZero( img ), 
+								translation ), 
+						img ) );
 	}
 
 
@@ -78,7 +96,8 @@ public class Ex01_TranformPointsAndImages
 	{
 		MixedTransform inverseTransform = new MixedTransform( 2, 2 );
 		// why do we need to set the inverse translation?
-		// see this blog post: TODO
+		// see these slides:
+		// https://github.com/bogovicj/transforms_tutorial/blob/master/resources/2019_DAIS.pdf 
 		inverseTransform.setInverseTranslation( translation );
 
 		MixedTransform forwardTransform = new MixedTransform( 2, 2 );
@@ -101,7 +120,6 @@ public class Ex01_TranformPointsAndImages
 	 * 
 	 * 
 	 */
-
 	public < T extends RealType< T > > void realTransform( RandomAccessibleInterval< T > img )
 	{
 		AffineTransform2D affine = new AffineTransform2D();
@@ -109,8 +127,17 @@ public class Ex01_TranformPointsAndImages
 		affine.translate( 99.5, -57.8 );
 		affine.rotate( Math.PI / 8 );
 
-		RealRandomAccessible< T > interpolatedImg = Views.interpolate( Views.extendZero( img ), new NLinearInterpolatorFactory< T >() );
-		IntervalView< T > transformedImage = Views.interval( Views.raster( RealViews.affine( interpolatedImg, affine ) ), img );
+		// use linear interpolation
+		RealRandomAccessible< T > interpolatedImg = 
+				Views.interpolate( 
+						Views.extendZero( img ), 
+						new NLinearInterpolatorFactory< T >() );
+
+		// transform the interpolated image
+		// and define the grid ("Interval") of the new image
+		IntervalView< T > transformedImage = Views.interval( 
+				Views.raster( 
+						RealViews.affine( interpolatedImg, affine ) ), img );
 
 		RealPoint q = new RealPoint( 2 );
 		affine.apply( p, q );
@@ -119,5 +146,47 @@ public class Ex01_TranformPointsAndImages
 		helper.displayImageAndPoint( transformedImage, q, "Affine transformed boats, with a point roi" );
 	}
 
+
+	/**
+	 * Fill in this method as an exercise.
+	 * 
+	 * This method should return a new {@link RandomAccessibleInterval} with the same field of view
+	 * as the input image, but with a new relative spacing.
+	 * 
+	 * Relative spacings greater than one (1.0) mean the image will be sampled more sparsely (and there will be
+	 * fewer pixel values per dimension).  Relative spacing less than one (1.0) mean the output image
+	 * will be sampled more densely (and there will be more pixel values per dimension).
+	 * 
+	 * @param img the input image
+	 * @param relativeSpacingX the relative spacing along the x (first) dimension
+	 * @param relativeSpacingY the relative spacing along the y (second) dimension
+	 * @return the resampled image
+	 */
+	public < T extends RealType< T > > RandomAccessibleInterval<T> resample2d( 
+			final RandomAccessibleInterval< T > img,
+			final double relativeSpacingX, final double relativeSpacingY )
+	{
+		/*
+		 * TODO - code this method as an exercise
+		 * 
+		 * Outline:
+		 * 1) Build an transformation that changes the relative spacing
+		 * 2) Apply the transform to the image.
+		 * 3) Define the grid appropriately by:
+		 *    a) Transforming the min/max of the input interval with your transform.
+		 *    b) Creating an interval with the new min/max (see "FinalInterval").
+		 *    c) Rasterize (Views.raster) and set the bounds (Views.interval)
+		 */
+
+		return null;
+	}
+
+	public < T extends RealType< T > > void testResample2d() throws IOException
+	{
+		RandomAccessibleInterval<UnsignedByteType> img = helper.openBoats();
+		RandomAccessibleInterval<UnsignedByteType> resampledImg = resample2d( img, 1.2, 0.8 );
+		ij.ui().show( "original boats", img );
+		ij.ui().show( "resampled boats", resampledImg );
+	}
 
 }
--- src/main/java/net/imglib2/examples/Helpers.java
+++ src/main/java/net/imglib2/examples/Helpers.java
@@ -1,10 +1,13 @@
 package net.imglib2.examples;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import ij.ImagePlus;
+import net.imagej.Dataset;
 import net.imagej.ImageJ;
 import net.imagej.display.ImageDisplay;
 import net.imagej.overlay.PointOverlay;
@@ -21,6 +24,8 @@ import net.imglib2.view.Views;
 public class Helpers
 {
 	protected ImageJ ij;
+
+	File localBoats = new File( "resources/boats.tif" );
 
 	public < T extends RealType< T > > void displayImageAndPoint( RandomAccessibleInterval< T > img, RealLocalizable pt, String name )
 	{
@@ -40,13 +45,30 @@ public class Helpers
 	}
 
 
+	/**
+	 * Read from imagej.nih.gov the first we read the image,
+	 * but save it locally so that opening is faster 
+	 * on subsequent runs.
+	 */
 	@SuppressWarnings( "unchecked" )
 	public RandomAccessibleInterval< UnsignedByteType > openBoats() throws IOException
 	{
-		// hyperslice because 'boats' opens as an RGB image, with all channels identical 
-		return Views.hyperSlice( 
-				( RandomAccessibleInterval< UnsignedByteType > ) ij.io().open( "https://imagej.nih.gov/ij/images/boats.gif" ), 2, 0);
-		
+		String path = "https://imagej.nih.gov/ij/images/boats.gif";
+		if( localBoats.exists())
+		{
+			Object obj =  ij.io().open( localBoats.getPath() );
+			// hyperslice because 'boats' opens as an RGB image, with all channels identical 
+			return Views.hyperSlice( (RandomAccessibleInterval<UnsignedByteType>)obj, 2, 0 );
+		}
+		else
+		{
+			Object obj = ij.io().open( path );
+			ij.io().save( (Dataset)obj, localBoats.getAbsolutePath());
+
+			// hyperslice because 'boats' opens as an RGB image, with all channels identical 
+			RandomAccessibleInterval<UnsignedByteType> imgout = Views.hyperSlice( (RandomAccessibleInterval<UnsignedByteType>)obj, 2, 0 );
+			return imgout;
+		}
 	}
 	
 	public static <T extends NativeType<T> & RealType<T>> RandomAccessibleInterval<T> copy( 
